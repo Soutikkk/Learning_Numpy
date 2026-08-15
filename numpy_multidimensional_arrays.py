568e810a10afd6741b313ad29c36cb3522927960
Soutikkk <soutikmandal505@gmail.com>
2026-08-15 10:56:51 +0530

    Learning Multidimensional Arrays in Numpy

---
+++ numpy_multidimensional_arrays.py
@@ -0,0 +1,36 @@
+# working with numpy multidimensional arrays
+
+import numpy as np
+
+array0 = np.array('A') # zero dimensional array
+print(array0.ndim) # prints number of dimensions
+print(array0.shape) # prints tuple of integers (depth, rows, columns)
+
+array1 = np.array(['A', 'B', 'C']) # one dimensional array
+print(array1.ndim) # prints number of dimensions
+print(array1.shape) # prints tuple of integers (depth, rows, columns)
+
+array2 = np.array([['A', 'B', 'C'], 
+                   ['D', 'E', 'F'],
+                   ['G', 'H', 'I']]) # two dimensional array
+print(array2.ndim) # prints number of dimensions
+print(array2.shape) # prints tuple of integers (depth, rows, columns)
+
+array3 = np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']],
+                   [['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R']],
+                   [['S', 'T', 'U'], ['V', 'W', 'X'], ['Y', 'Z', '_']]]) # three dimensional array
+print(array3.ndim) # prints number of dimensions
+print(array3.shape) # prints tuple of integers (depth, rows, columns)
+print(array3[0][0][1]) # multidimensional indexing (layer, row, element)
+print(array3[0][0][2]) # multidimensional indexing (layer, row, element)
+print(array3[0][1][0]) # multidimensional indexing (layer, row, element)
+print(array3[0][2][0]) # multidimensional indexing (layer, row, element)
+print(array3[1][1][1]) # multidimensional indexing (layer, row, element)
+print(array3[0][0][0]) # multidimensional indexing (layer, row, element)
+
+# form 3 letter word using string concatenation
+word1 = array3[0, 0, 0] + array3[2, 0, 0] + array3[2, 0 ,0] # ASS
+word2 = array3[0, 1, 0] + array3[0, 0, 0] + array3[2, 2, 0] # DAY
+
+print(word1)
+print(word2)
