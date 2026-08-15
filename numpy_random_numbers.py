8a4a3c537c6fa2352475cb1ee2bae75e8ebaa4e8
Soutikkk <soutikmandal505@gmail.com>
2026-08-15 11:01:09 +0530

    Learning Random_Numbers in Numpy

---
+++ numpy_random_numbers.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# sets the random number generator
+rng = np.random.default_rng()
+
+# prints the random numbers
+# parameters: starting, end (exclusive), size
+print(rng.integers(low=1, high=101, size=(3, 2)))
+
+# uniform = uniform distribution (floating point number)
+print(np.random.uniform(low=-1, high=1, size=(3,3)))
+
+# shuffling an array
+array = np.array([1, 2, 3, 4, 5])
+rng.shuffle(array)
+print(array)
+
+# randomly choose a string from an array
+fruits = np.array(["apple", "orange", "banana", "coconut", "pineapple"])
+fruit = rng.choice(fruits, size=(3,2))
+print(fruit)
